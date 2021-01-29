f2837058d9880c64f31160a6a20f1003f95f6b1e
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-29 20:57:30 +0100

    add sass

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const game = document.querySelector('.main__game');
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 function showBalls() {
+  let html = '';
   for (let i = 0; i < 20; i++) {
-    console.log(getRandomNumber(100));
+    html += `<div class="main__game--ball"> ${getRandomNumber(100)}</div>`;
   }
+  game.innerHTML += html;
 }
 showBalls();
