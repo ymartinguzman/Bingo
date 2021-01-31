3b6c38d396d431cc2b0654c1b829882a962bbeed
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-31 20:15:12 +0100

    add click event to show ball one by one

--- js/index.js
+++ js/index.js
@@ -1,15 +1,34 @@
 'use strict';
 
+const balls = document.querySelector('.main__balls');
+const newBall = document.querySelector('.header__game-ball');
 const game = document.querySelector('.main__game');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+
 function showBalls() {
-  let html = '';
-  for (let i = 0; i < 20; i++) {
-    html += `<div class="main__game--ball"> ${getRandomNumber(100)}</div>`;
+  let htmlBalls = '';
+
+  for (let i = 0; i < 1; i++) {
+    htmlBalls += `<div class="main__balls--ball"> ${getRandomNumber(
+      100
+    )}</div>`;
   }
-  game.innerHTML += html;
+  balls.innerHTML += htmlBalls;
 }
-showBalls();
+
+newBall.addEventListener('click', showBalls);
+
+function bingoCard() {
+  let htmlBingo = '';
+
+  for (let i = 0; i < 20; i++) {
+    htmlBingo += `<div class="main__game--ball"> ${getRandomNumber(100)}</div>`;
+  }
+  game.innerHTML += htmlBingo;
+  console.log(htmlBingo);
+}
+
+bingoCard();
