e9ab9804420459fc9b5d8263c83982cc093805a7
ymartinguzman <yamiramartinguzman@gmail.com>
2021-02-01 10:52:42 +0100

    add function to set interval and show balls

--- js/index.js
+++ js/index.js
@@ -3,43 +3,51 @@
 const balls = document.querySelector('.main__balls');
 const newBall = document.querySelector('.header__game-ball');
 const game = document.querySelector('.main__game');
-const play = document.querySelector('header__game-play');
-console.log(play);
+const play = document.querySelector('.header__game-play');
 
 //Get random number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+const showArrayNumbers = [];
+const cardArrayNumbers = [];
 //Random numbers in game
-function showBalls() {
+function showBalls(ev) {
   let htmlBalls = '';
 
   for (let i = 0; i < 1; i++) {
-    htmlBalls += `<div class="main__balls--ball"> ${getRandomNumber(
-      100
-    )}</div>`;
+    const number = getRandomNumber(100);
+    htmlBalls += `<div class="main__balls--ball"> ${number}</div>`;
+    showArrayNumbers.push(number);
   }
+
   balls.innerHTML += htmlBalls;
 }
 
 //Random card
 function bingoCard() {
   let htmlBingo = '';
-
   for (let i = 0; i < 20; i++) {
-    htmlBingo += `<div class="main__game--ball"> ${getRandomNumber(100)}</div>`;
+    const number = getRandomNumber(100);
+    htmlBingo += `<div class="main__game--ball"> ${number}</div>`;
+    cardArrayNumbers.push(number);
   }
   game.innerHTML += htmlBingo;
-  console.log(htmlBingo);
 }
 
 bingoCard();
 
 //SetInterval
 
-// setInterval(showBalls, 2000);
+function autoBalls() {
+  setInterval(showBalls, 2000);
+  // if (showArrayNumbers.length === 10) {
+  //   clearInterval(setInterval);
+  // }
+  // console.log(clearInterval(setInterval));
+}
 
 //Events
 newBall.addEventListener('click', showBalls);
-play.addEventListener('click', setInterval);
+play.addEventListener('click', autoBalls);
